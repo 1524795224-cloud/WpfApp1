ead15ad1e37e65fb0abb353ffa79bdd36858c92c
1524795224-cloud <1524795224@qq.com>
2026-08-21 13:53:22 +0800

    控制台输出日志实现

---
+++ Service/Communication/Log/AppConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Service.Communication.Log
+{
+    public class AppConfig
+    {
+
+        /// <summary>环境名称</summary>
+        public string Environment { get; set; } = "Development";
+
+        /// <summary>日志配置</summary>
+        public LoggerConfig LoggingConfig { get; set; } =
+            new LoggerConfig
+            {
+                EnableConsole = true,
+                LogDirectory = "Logs",
+                MinLogLevel = LogLevel.Debug,
+                LogFileName = "app.log",
+                MaxFileSizeMB = 10
+            };
+
+
+    }
+    public static class AppConfiguration
+    {
+        #region 公共属性
+        /// <summary>
+        /// 当前应用程序配置
+        /// </summary>
+        public static AppConfig Current { get; set; } = new AppConfig();
+        #endregion
+    }
+}
---
+++ Service/Communication/Log/LogLevel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Service.Communication.Log
+{
+    /// <summary>
+    /// 日志等级枚举
+    /// </summary>
+    public enum LogLevel
+    {
+        Debug,    // 调试信息，详细信息
+        Info,     // 一般信息
+        Warning,  // 警告信息
+        Error,    // 错误信息
+        Fatal     // 致命错误
+    }
+    public enum LogCategory
+    {
+        Startup,    // 应用启动/关闭
+        Module,     // 模块加载
+        Thread,     // 线程管理
+        Service,    // 服务启动
+        Database,   // 数据库操作
+        UI,         // UI相关
+        Config,     // 配置加载
+        Security,   // 安全/登录
+        Business,   // 业务逻辑
+        System      // 系统事件
+    }
+}
